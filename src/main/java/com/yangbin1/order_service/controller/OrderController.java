cb127c101082fb4241bf7c5f96d985873a8c9822
yangbin1 <yangbin199191@163.com>
2019-05-20 09:37:03 +0800

    333

--- src/main/java/com/yangbin1/order_service/controller/OrderController.java
+++ src/main/java/com/yangbin1/order_service/controller/OrderController.java
@@ -2,9 +2,7 @@ package com.yangbin1.order_service.controller;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.yangbin1.order_service.service.ProductOrderService;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/v1/order")
@@ -21,8 +18,8 @@ public class OrderController {
     @Autowired
     private ProductOrderService productOrderService;
 
-    @Autowired
-    private StringRedisTemplate redisTemplate;
+//    @Autowired
+//    private StringRedisTemplate redisTemplate;
 
     @RequestMapping("save")
     @HystrixCommand(fallbackMethod = "saveOrderFail")
@@ -36,18 +33,18 @@ public class OrderController {
     //方法签名和api方法一致
     private Object saveOrderFail(int userId,int productId, HttpServletRequest request){
 
-        String saveOrderKey = "save-order";
-        //监控报警
-        String sendValue = redisTemplate.opsForValue().get(saveOrderKey);
-        String ip = request.getRemoteAddr();
-        new Thread( ()->{
-            if (StringUtils.isBlank(sendValue)){
-                System.out.println("发送短信,ip地址"+ ip);
-                redisTemplate.opsForValue().set("saveOrderKey","save-order-fail",20, TimeUnit.SECONDS);
-            }else{
-                System.out.println("已经发送过短信");
-            }
-        }).start();
+//        String saveOrderKey = "save-order";
+//        //监控报警
+//        String sendValue = redisTemplate.opsForValue().get(saveOrderKey);
+//        String ip = request.getRemoteAddr();
+//        new Thread( ()->{
+//            if (StringUtils.isBlank(sendValue)){
+//                System.out.println("发送短信,ip地址"+ ip);
+//                redisTemplate.opsForValue().set("saveOrderKey","save-order-fail",20, TimeUnit.SECONDS);
+//            }else{
+//                System.out.println("已经发送过短信");
+//            }
+//        }).start();
 
 
         Map<String,Object> msg = new HashMap<>();
